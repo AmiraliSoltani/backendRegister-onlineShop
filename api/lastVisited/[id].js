f4cb4526eb16dfec12ee123c256eb8c513547a8a
Amirali Soltani <asoltani7@myseneca.ca>
2024-10-09 09:56:49 -0700

    vercel3

--- api/lastVisited/[id].js
+++ api/lastVisited/[id].js
@@ -73,6 +73,7 @@ const handler = async (req, res) => {
   const productId = req.query.id; // Extract dynamic ID from the URL
   console.log("userId",userId)
   console.log("productId",productId)
+  console.log("req.query",req.query)
 
   // Handle PUT requests to add last visited product
   if (req.method === 'PUT') {
